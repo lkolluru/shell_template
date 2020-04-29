43d8f142fe3f924afabf1afc19e598f86bacbea0
Kolluru, Lakshmi <lkolluru@pch.com>
2020-04-28 23:41:32 -0400

    hybrid scoring

--- scripts/bash/process/oflnsel_hybridscoring_compute_process.sh
+++ scripts/bash/process/oflnsel_hybridscoring_compute_process.sh
@@ -1,94 +1,113 @@
+#!/bin/bash  
+
 #!/bin/bash
 
-set -euo pipefail;
-source /mapr/JMAPRCLUP01.CLASSIC.PCHAD.COM/codebase/calcengine/$1/oflnsel/config/oflnsel_env.sh ;
-source ${PROCESS_SHELL_TEMPLATE_SCRIPT} ;
-
-#this is a file watcher to check sas scoring files arrived
-#sends email when file available and also sends email if the file not arrived at 12 PM
-function checkTrigFile() {
-info_log "inside trig fie location "
-trap send_mainscript_failure_email  ERR
-
-if [ `ls "$HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR" | wc -l` != "0" ]; then
-        info_log "below is the files on import trigger dir ${HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR} "
-        ls -ltr "${HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR}"
-        info_log "$(ls ${HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR})"
-        if [ `ls "$HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR" | wc -l` != "1" ]; then
-                error_log "there are two or more trigger files" 
-                mail -s "${email_subject_env}: ${log_subject_area}: ATTENTION - More than one trigger file" ${failure_to_email}  <<< "Please check. There are two or more trigger files. Triggers  are at $HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR";
-        mail -v -r noreply@pch.com -s "${email_subject_env}: ${log_subject_area}: ATTENTION - More than one trigger files for Import"  ${to_phone} <<< "Please check. There are two or more trigger files. Triggers  are at $HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR";
+#Set Global Variables
+set -uo pipefail
+set -E
+set -o errtrace
+#######################################
+# OFLNSEL WORKFLOW RE_DIRECT Repository Module
+# Process Modules:
+#   main -- evaluate the steps files and execute the steps
+#   test1 -- evaluate all the previous executions and archive the required logs
+#   test2 -- evaluate all the previous executions and archive the required logs
+#######################################
+case ${2} in
+main)
+        source /mapr/JMAPRCLUP01.CLASSIC.PCHAD.COM/codebase/calcengine/${1}/oflnsel/config/oflnsel_env.sh
+        ;;
+test1)
+        source /mapr/JMAPRCLUP01.CLASSIC.PCHAD.COM/codebase/calcengine/${1}/oflnsel/config/oflnsel_env_test1.sh
+        ;;
+test2)
+        source /mapr/JMAPRCLUP01.CLASSIC.PCHAD.COM/codebase/calcengine/${1}/oflnsel/config/oflnsel_env_test2.sh
+        ;;
+*)
         exit 1
+        ;;
+esac
+
+source ${PROCESS_SHELL_TEMPLATE_SCRIPT}
+source ${ARCHIVE_LOG_SHELL}
+source ${FILE_HANDLER_SCRIPT}
+trap cleanup SIGINT SIGHUP SIGTERM EXIT
+trap 'gen_prss_error ${LINENO} ${?}' ERR
+
+#Functions
+
+#######################################
+# OFLNSEL Process Functions Repository Module
+# Process Modules:
+#   runModule -- evaluate the steps files and execute the steps
+#   run_archive_logs -- evaluate all the previous executions and archive the required logs
+#   main -- evaluate all the previous executions and archive the required logs
+#   preproces -- evaluate all the previous executions and archive the required logs
+#######################################
+
+function runModule() {
+
+        if ! test_path ${PROJ_STEPS_RUNFILE_HYBRIDSCORING_COMPUTE}; then
+                return 1
         fi
 
-        SAS_SCORING_FILE_NM=`ls $HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR`
-        info_log "scoring file: $SAS_SCORING_FILE_NM  received.  Sending email & proceeding with process" 
-        mail -s "${email_subject_env}: ${log_subject_area} : RECEIVED - COMPUTE Files" -a "$HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR/$SAS_SCORING_FILE_NM" ${success_to_email}  <<< "Proceeding with ${log_subject_area} import &  compute  processing"
-        mail -v -r noreply@pch.com -s "${email_subject_env}: RECEIVED - Import Files"  ${to_phone} <<< "Proceeding with ${log_subject_area} import &  compute  processing"
-		info_log "moving the trig file from  ${HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR} to ${HYBRIDSCORING_FILE_WATCH_IMPORT_COMPLETE_DIR} "
-        mv -f ${HYBRIDSCORING_FILE_WATCH_IMPORT_READY_DIR}/oflnsel_export_trigger*  ${HYBRIDSCORING_FILE_WATCH_IMPORT_COMPLETE_DIR}
+        mapfile -t hybridscoring_file_items <${PROJ_STEPS_RUNFILE_HYBRIDSCORING_COMPUTE}
 
-fi
+        for line in ${hybridscoring_file_items[@]}; do
+
+                cmnt=$(echo $line | awk '{ print substr($1,0,1) }') #check whether step commented
+
+                if [ "$cmnt" == "#" ]; then
+
+                        info_log "$FUNCNAME:Skipping the step $line\n"
+                        continue
+
+                elif [ "$cmnt" == "" ]; then
+
+                        info_log "$FUNCNAME:Skipping empty line\n"
+
+                else
+
+                        step_name="${PROJ_BASH_POSTSCORING_DIR}/${line}"
+
+                        if ! test_path ${step_name}; then
+                                return 1
+                        fi
+
+                        bash "${PROJ_BASH_POSTSCORING_DIR}/${line}"
+
+                        subprocess_return_code=$?
+
+                        [ ${subprocess_return_code} -ne 0 ] && fatal_log "$FUNCNAME: ${step_name} failed with a error code from the step shell with ${subprocess_return_code}" && exit 1
+
+                        [ ${subprocess_return_code} -eq 0 ] && info_log "$FUNCNAME: ${step_name} completed successfully with ${subprocess_return_code}"
+
+                        unset step_name
+
+                fi
+
+        done
 
 }
 
+function main() {
 
-#this is the main function which executes each step in RUNFILE
-function runModule() {
-cat "${PROJ_STEPS_RUNFILE_HYBRIDSCORING_COMPUTE}" |
-   while read line
-   do
-     cmnt=`echo $line | awk '{ print substr($1,0,1) }'` #check whether step commented
-     if [ "$cmnt" == "#" ]; then
-        info_log "Skipping the step $line\n"
-        continue
-     elif [ "$cmnt" == "" ]; then
-        info_log "Skipping empty line\n"
-     else
-   	if [ -e "${PROJ_BASH_POSTSCORING_DIR}/${line}" ]; then #check for step shell name existence
-		bash "${PROJ_BASH_POSTSCORING_DIR}/${line}"
-	else 
-		info_log "${PROJ_BASH_POSTSCORING_DIR}/${line} doesn't seem to exists\n"
-		touch $temp_touch_file ;
-		send_mainscript_failure_email
-		break
-	fi
-     fi
-   done
+        export HADOOP_USER_CLASSPATH_FIRST=true
+
+        info_log "$FUNCNAME:Command executed: ${0}"
+
+        runModule
+
+        main_return_code=$?
+
+        [ $main_return_code -ne 0 ] && fatal_log "$FUNCNAME:unable to complete successfully failing the process" && exit ${main_return_code}
+
+        return ${main_return_code}
+
 }
-
-
-trap cleanup SIGINT SIGHUP SIGTERM EXIT
 
 #Main Program
 
-return_exit_code=0;
-#Setup new or edit cron log file.
-prepare_cronlog_file
+prepare_log_file
 
-# this touch file to trigger success email only after completion of all steps
-# depricated only for legacy used cases.
-
-temp_touch_file="${log_directory}/tmp_status_$(date +"%Y_%m_%d_%H_%M_%S")";
-
-info_log "Command executed: ${0}" 2>&1 | tee -a ${log_file};
-
-#checking preselectionfile
-checkTrigFile
-bash ${ARCHIVE_LOG_SHELL}
-export HADOOP_USER_CLASSPATH_FIRST=true;
-#Send success email otherwise exit
-if [ "$?" == "0" ]; then 
-	info_log "dependency objects test1: for hybrid process received.  Sending email & proceeding with process" 2>&1 | tee -a ${log_file}
-	runModule 2>&1  | tee -a ${log_file};
-	if [ "$?"="0" ] && [ ! -e "$temp_touch_file" ]  ; then
-        	send_success_email
-	fi
-
-else 
-        info_log "dependency objects test1: for hybrid process is yet to received.  Sending email & existing the application" 2>&1 | tee -a ${log_file}
-	exit 1
-fi
-	
-
-exit ;
+main 2>&1 | tee -a ${log_file}
