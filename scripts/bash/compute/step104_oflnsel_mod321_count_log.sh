d680e47039c7e9298f02da03372eb3bc7c8fea45
Kolluru, Lakshmi <lkolluru@pch.com>
2020-04-29 11:36:48 -0400

    final fixes

--- scripts/bash/compute/step104_oflnsel_mod321_count_log.sh
+++ scripts/bash/compute/step104_oflnsel_mod321_count_log.sh
@@ -18,15 +18,15 @@ load_control_table() {
     model_count_logging_file="${MODEL_COUNTS_LOG_FILE}"
     model_count=$(beehive "select count(*) from ${MODEL_COUNT_MOD321_TABLE}")
 
-    info_log "Command executed: ${0}" 2>&1 | tee -a ${step_log_file}
+    info_log "$FUNCNAME:Command executed: ${0}" 
 
     if [ "${model_count}" != "" ] && [ "${model_count}" != "0" ]; then
         echo "${model_name},$(date '+%Y-%m-%d %H:%M:%S'),${model_count},${load_env}" >>${model_count_logging_file}
 
-        info_log "Entry made into model_count_log table" 2>&1 | tee -a ${step_log_file}
+        info_log "$FUNCNAME:Entry made into model_count_log table" 
     else
-        error_log "Hive table: ${MODEL_COUNT_MOD321_TABLE}  is empty or count retrieval failed. Log entry not made.  please check" 2>&1 | tee -a ${step_log_file}
-        exit 1
+        error_log "$FUNCNAME:Hive table: ${MODEL_COUNT_MOD321_TABLE}  is empty or count retrieval failed. Log entry not made.  please check" 
+        return 1
     fi
 
 }
--- scripts/bash/compute/step105_oflnsel_modsro_count_log.sh
+++ scripts/bash/compute/step105_oflnsel_modsro_count_log.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #Set Global Variables
 set -uo pipefail
@@ -10,28 +10,39 @@ trap 'gen_step_error ${LINENO} ${?}' ERR
 
 #Main Program
 
-#CHANGE FOLLOWING VARIABLES ACCORDING TO ENV
-export HIVE_SHELLFILE_NM=$(basename ${0});
+load_control_table() {
 
-export model_name="modsro";
-export load_env="${CODE_ENV_FLAG}";
-export model_count_logging_file="${MODEL_COUNTS_LOG_FILE}";
-export model_count=`/usr/local/bin/hive-bigdata -e "select count(*) from ${MODEL_COUNT_MODSRO_TABLE}"`;
+    HIVE_SHELLFILE_NM=$(basename ${0})
+    model_name="modsro"
+    load_env="${CODE_ENV_FLAG}"
+    model_count_logging_file="${MODEL_COUNTS_LOG_FILE}"
+    model_count=$(beehive "select count(*) from ${MODEL_COUNT_MODSRO_TABLE}")
+
+    info_log "Command executed: ${0}" 
+
+    if [ "${model_count}" != "" ] && [ "${model_count}" != "0" ]; then
+        echo "${model_name},$(date '+%Y-%m-%d %H:%M:%S'),${model_count},${load_env}" >>${model_count_logging_file}
+
+        info_log "$FUNCNAME:Entry made into model_count_log table" 
+    else
+        error_log "$FUNCNAME:Hive table: ${MODEL_COUNT_MODSRO_TABLE}  is empty or count retrieval failed. Log entry not made.  please check" 
+        return 1
+    fi
+
+}
+
+main() {
+    info_log "$FUNCNAME:Command executed: ${0}"
+
+    if ! load_control_table; then
+
+        exit 1
+
+    fi
+
+}
 
 #Setup new or edit log file.
-prepare_log_file;
+prepare_log_file
 
-info_log "Command executed: ${0}" 2>&1 | tee -a ${step_log_file}
-
-if [ "${model_count}" != "" ] && [ "${model_count}" != "0" ]; then
-echo "${model_name},$(date '+%Y-%m-%d %H:%M:%S'),${model_count},${load_env}" >> ${model_count_logging_file}
-
-info_log "Entry made into model_count_log table" 2>&1 | tee -a ${step_log_file};
-else
-error_log "Hive table: ${MODEL_COUNT_MODSRO_TABLE}  is empty or count retrieval failed. Log entry not made.  please check" 2>&1 | tee -a ${step_log_file};
-exit 1
-fi
-
-
-exit $?  ;
-
+main 2>&1 | tee -a ${step_log_file}
